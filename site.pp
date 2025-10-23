a0f4b87d06796f4fb88d49ba85e756d3ec290588
ShrimpRick <67194155+ShrimpRick@users.noreply.github.com>
2025-10-23 16:09:05 +0200

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,5 +1,29 @@
+# /etc/puppetlabs/code/environments/production/manifests/site.pp
+
 node default {
-  class { 'fetch_api':
-    api_url => 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true',
+  # Zorg dat het env-bestand bestaat
+  file { '/etc/fetch_api.env':
+    ensure => present,
+  }
+
+  # Maak het script dat de API-call doet
+  file { '/usr/local/bin/fetch_api.sh':
+    ensure  => file,
+    mode    => '0755',
+    content => @("API")
+      #!/bin/bash
+      source /etc/fetch_api.env
+      echo "Fetching data from \$API_URL..."
+      curl -s "\$API_URL" -o /tmp/api_result.json
+      echo "Saved to /tmp/api_result.json"
+      | API
+  }
+
+  # Run het script
+  exec { 'fetch_api':
+    command => '/usr/local/bin/fetch_api.sh',
+    path    => ['/bin','/usr/bin'],
+    refreshonly => false,
+    require => File['/usr/local/bin/fetch_api.sh'],
   }
 }
