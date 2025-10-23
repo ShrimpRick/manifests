332bd44ab244272fbde797ee4d8ea445413f9893
ShrimpRick <67194155+ShrimpRick@users.noreply.github.com>
2025-10-23 16:57:47 +0200

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,16 +1,12 @@
-# /etc/puppetlabs/code/environments/production/manifests/site.pp
-
 node default {
-  # Zorg dat het env-bestand bestaat
   file { '/etc/fetch_api.env':
     ensure => present,
   }
 
-  # Maak het script dat de API-call doet
   file { '/usr/local/bin/fetch_api.sh':
     ensure  => file,
     mode    => '0755',
-    content => @("API")
+    content => @("API"/L)
       #!/bin/bash
       source /etc/fetch_api.env
       echo "Fetching data from \$API_URL..."
@@ -19,11 +15,10 @@ node default {
       | API
   }
 
-  # Run het script
   exec { 'fetch_api':
-    command => '/usr/local/bin/fetch_api.sh',
-    path    => ['/bin','/usr/bin'],
+    command     => '/usr/local/bin/fetch_api.sh',
+    path        => ['/bin','/usr/bin'],
     refreshonly => false,
-    require => File['/usr/local/bin/fetch_api.sh'],
+    require     => File['/usr/local/bin/fetch_api.sh'],
   }
 }
